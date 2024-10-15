79e3d083b31caa3c6cddc1db29dc1e7686ad80ff
Toee Thiha Kyaw <sanjikun823@gmail.com>
2024-10-15 18:52:56 +0630

    JS : Finish Creating DOM Elements

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let start = document.querySelector(".start-btn");
+let stop = document.querySelector(".stop-btn");
+let reset = document.querySelector(".reset-btn");
+let close = document.querySelector(".close-btn");
+let errorBox = document.querySelector(".error-box");
+let errorMsg = document.querySelector(".error-message");
+let resultBox = document.querySelector(".result-box");
+
+errorBox.style.zIndex = "1";
